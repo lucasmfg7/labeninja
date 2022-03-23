4f1c43a09678a22190b709b0b8503d7143398cdc
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-03-23 11:57:06 -0300

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 const App = () => {
+  const [currentScreen, setCurrentScreen] = useState("home");
+
+  function selectPage() {
+    switch (currentScreen) {
+      case "home":
+        return;
+      case "list":
+        return;
+      case "create":
+        return;
+      case "cart":
+        return;
+      default:
+        return;
+    }
+  }
+
   return (
     <>
+      <Header />
       App
       <GlobalStyle />
     </>
---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background: var(--purple);
+  padding: 1rem 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  a {
+    text-decoration: none;
+    color: var(--background);
+
+    &:hover {
+    }
+  }
+
+  button {
+    border: 0;
+    padding: 0.5rem 0.75rem;
+    border-radius: 0.25rem;
+    background: var(--background);
+    color: var(--purple);
+    font-weight: 600;
+    transition: border 0.2s, background-color 0.2s, color 0.2s;
+
+    &:hover {
+      border: 1px solid var(--background);
+      background-color: var(--purple);
+      color: var(--background);
+    }
+  }
+`;
+
+export const Header = () => {
+  return (
+    <Container>
+      <h1>
+        <a href="#">LabeNinjas</a>
+      </h1>
+      <div>
+        <button>Carrinho</button>
+      </div>
+    </Container>
+  );
+};
