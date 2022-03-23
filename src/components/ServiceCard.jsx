28a9b2c2fe7a9e32573c4a88b64aa05aa3be45d2
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-03-23 14:58:44 -0300

    ServiceCard

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background: var(--purple);
+  color: var(--background);
+  padding: 1rem 2rem;
+  border-radius: 0.25rem;
+  text-align: center;
+
+  h4 {
+    margin-bottom: 0.5rem;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  margin-top: 0.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+
+  button {
+    border: 1px solid var(--purple);
+    padding: 0.5rem 0.75rem;
+    border-radius: 0.25rem;
+    background: var(--background);
+    color: var(--purple);
+    font-weight: 600;
+    transition: border 0.2s, background-color 0.2s, color 0.2s;
+
+    &:hover {
+      border: 1px solid var(--background);
+      background-color: var(--purple);
+      color: var(--background);
+    }
+  }
+`;
+
+export const ServiceCard = () => {
+  return (
+    <Container>
+      <h4>Título do Serviço</h4>
+      <p>
+        <strong>Preço: </strong>R$ 12
+      </p>
+      <p>
+        <strong>Prazo: </strong>12/12/2022
+      </p>
+      <ButtonContainer>
+        <button>Detalhes</button>
+        <button>Adicionar</button>
+      </ButtonContainer>
+    </Container>
+  );
+};
